205c141a6a03e4579ce3cd95b846879b93d8d88a
sudirboyy <sudirboyy@gmail.com>
2023-04-14 10:35:21 +0700

    2

--- LinearSearch_178/LinearSearch_178.cpp
+++ LinearSearch_178/LinearSearch_178.cpp
@@ -5,3 +5,16 @@ int arr[20]; // Array to be searched
 int n; // Number
 int i; //
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+
+}
