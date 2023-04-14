a429785e18b57807e1e7b8178185a31eb9629726
sudirboyy <sudirboyy@gmail.com>
2023-04-14 10:39:22 +0700

    3

--- LinearSearch_178/LinearSearch_178.cpp
+++ LinearSearch_178/LinearSearch_178.cpp
@@ -17,4 +17,13 @@ void input()
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
 
+	// Accept array element
+	cout << "\n--------------------\n";
+	cout << "Enter array element\n";
+	cout << "\n--------------------\n";
+	for ( i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
